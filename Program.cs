98af252ebea622a8ad4b1ff0f55f8a99a61dc1a2
Martynas Gelumbauskas <martynas.gelumbauskas@gmail.com>
2020-08-27 17:36:29 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Kartotinis
+{
+    class Program
+    {
+        public static void CalculateKartotinis()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Sku");
+                }
+
+                else if (i % 3 != 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("ba");
+                }
+
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("Skuba");
+                }
+
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            CalculateKartotinis();
+        }
+    }
+}
